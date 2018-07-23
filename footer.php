e2a75726ddc9176fb2b80ba9f084150dd92963f7
frankline kelvin kipruto <32270980+Frankline-Kelvin@users.noreply.github.com>
2018-07-23 21:30:01 +0300

    Initial commit

---
+++ footer.php
@@ -0,0 +1,60 @@
+<footer class="page-footer" style="margin-top:0px;">
+        <div class="success-color" >
+            <div class="container-fluid container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h3 class="center">About Rehani Fresh</h3>
+                        <hr style="background-color: whitesmoke">
+                        <ul class="navbar-nav nav">
+                            <li>
+                                <a href="/about" class=" waves-effect waves-light">Company Information</a>
+                            </li>
+                            <li>Privacy Policy</li>
+                            <li>Terms of use</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <h3>Social</h3>
+                        <hr style="background-color: whitesmoke">
+                        <p> Stay up-to-date through our social media channels:</p>
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item">
+                                <a href="https://www.facebook.com/" style="background-color: transparent" class=" waves-effect waves-light">
+                                    <i class="fa fa-facebook" style="margin-right: 10px"></i>Facebook</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="https://www.twitter.com/" style="background-color: transparent" class=" waves-effect waves-light">
+                                    <i class="fa fa-twitter" style="margin-right: 10px"></i>Twitter</a>
+                            </li>
+                            <li class="nav-item">
+                                 <a href="https://www.instagram.com/" style="background-color: transparent" class=" waves-effect waves-light">
+                                <i class="fa fa-instagram" style="margin-right: 10px"></i>Instagram</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <h3>Important Links</h3>
+                        <hr style="background-color: whitesmoke">
+                        <ul>
+                            <li>
+                                <a href="#!">Products</a>
+                            </li>
+                            <li>
+                                <a href="#!">FAQ</a>
+                            </li>
+                            <li>
+                                <a href="#!">Business Partnerships</a>
+                            </li>
+                            <li>
+                                <a href="#!">About Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container-fluid success-color-dark">
+                <p>All Rights Reserved |© 2015 - <?php echo date('Y') ?> | Rehani Fresh</p>
+            </div>
+        </div>
+    </footer>
---
+++ index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Rehani Fresh</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<style>
+  .main{
+       background: url("img/products/farm.gif")no-repeat center center;
+       /* background-color: transparent; */
+    background-size: cover;
+    height: 80vh;
+  }
+  .farm{
+      position: absolute;
+margin-top: 15em;
+text-align: center;
+margin-left: 42%;
+  width: 20%;
+  background: rgba(79, 149, 0, 0.801);
+  padding: 0.4rem;
+  font-family: "Open Sans", sans-serif;
+  }
+</style>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark scrolling-navbar fixed-top" style="background-color: #84BD00;">
+        <a class="navbar-brand" href="#">
+            <!-- <strong>Rehani Fresh</strong> -->
+            <img src="img/logo1.png" alt="" height="50px">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav nav-flex-icons">
+                <li class="nav-item">
+                    <a class="nav-link">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">
+                        <i class="fa fa-instagram"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="main">
+    <div class="full-bg-img">
+        <div class="mask rgba-black-light flex-center">
+            <div class="container text-center white-text">
+                <div class="white-text text-center wow fadeInUp"  style="margin-top: 20em">
+                    <h2 class="animated fadeInUp"><strong>GLOW IN YOUR FRESHNESS</strong></h2>
+                    <!-- <h5 class="animated fadeInLeft">It will always stay visible on the top, even when you scroll down</h5> -->
+                    
+                    <p class="animated fadeInDown wow" data-wow-delay="0.6s">
+                         More importantly, as our name suggests, our goal is to offer FRESH produce straight<br> from our farms. You want fresh, a chance
+                        to bask in the glory of fresh produce?<br> Then the search is over for at Rehani your satisfaction is the prime priority.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <main>
+        <div class="flex-center">
+            <h3 style="color: #84BD00"><i class="fa fa-leaf animated fadeInUp" aria-hidden="true"></i>REHANI FRESH PRODUCTS</h3>
+        </div>
+        <div class="container">
+            <div class="row" style="margin-top: 10px">
+                <div class="col-md-3 wow fadeInLeft" data-wow-delay="0.6s">
+                    <div class="view overlay ">
+                        <img src="img/products/basil.jpg" class="img-fluid img-thumbnail" alt="" style="height: 200px;width: 255px">
+                        <div class="mask flex-center rgba-lime-strong">
+                            <p class="white-text">BASIL</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="view overlay ">
+                        <img src="img/products/chives.jpg" class="img-fluid img-thumbnail wow tada" data-wow-delay="0.6s" alt="" style="height: 200px;width: 255px">
+                        <div class="mask flex-center rgba-lime-strong">
+                            <p class="white-text">CHIVES</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="view overlay ">
+                        <img src="img/products/frenchbeans.jpg" class="img-fluid img-thumbnail wow tada" data-wow-delay="0.6s" alt="" style="height: 200px;width: 255px">
+                        <div class="mask flex-center rgba-lime-strong">
+                            <p class="white-text">FRENCH BEANS</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="view overlay ">
+                        <img src="img/products/mint.jpg" class="img-fluid img-thumbnail wow fadeInRight" data-wow-delay="0.6s" alt="" style="height: 200px;width: 255px">
+                        <div class="mask flex-center rgba-lime-strong">
+                            <p class="white-text">MINT</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row" style="margin-top: 10px">
+                    <div class="col-md-3">
+                        <div class="view overlay ">
+                            <img src="img/products/oregano.jpg" class="img-fluid img-thumbnail wow slideInLeft" data-wow-delay="0.6s" alt="" style="height: 200px;width: 255px">
+                            <div class="mask flex-center rgba-lime-strong">
+                                <p class="white-text">OREGANO</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="view overlay ">
+                            <img src="img/products/rosemary.jpg" class="img-fluid img-thumbnail wow slideInUp" data-wow-delay="0.6s" alt="" style="height: 200px;width: 255px">
+                            <div class="mask flex-center rgba-lime-strong">
+                                <p class="white-text">ROSEMARY</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="view overlay ">
+                            <img src="img/products/tarragon.jpg" class="img-fluid img-thumbnail wow slideInUp" data-wow-delay="0.6s" alt="" style="height: 200px;width: 255px">
+                            <div class="mask flex-center rgba-lime-strong">
+                                <p class="white-text">TARRAGON</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="view overlay ">
+                            <img src="img/products/thyme.jpg" class="img-fluid img-thumbnail wow slideInRight" data-wow-delay="0.6s" alt="" style="height: 200px;width: 255px">
+                            <div class="mask flex-center rgba-lime-strong">
+                                <p class="white-text">THYME</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section style="margin-top:50px;">
+           <div class="vision text-center white-text">
+               <h3 style="font-size: 24px;font-weight: bold">REHANI’S CORE VALUES</h3>
+               <p style="font-size: 20px;line-height: 1.3em;">
+                 Rehani Fresh solely believe in the fact that our clients are the roots to our success.<br>We therefore put our clients’ satisfaction
+                as the first priority for a smile for them is a smile for us.<br>We enforce our dedication to openness, honesty and availability at all times. This is following our belief in consistency
+                in our course.
+               </p>
+           </div>
+           <div class="row">
+<img src="img/products/farmer.jpg" alt="" class="vision-img">
+<div class="farm"><p class="text-white" style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;font-size: 30px;font-weight: bold;">Our Farm <hr style="margin-bottom:0px;background-color: #fff;"></p></div>
+           </div>
+        </section>
+    </main>
+    <?php
+    include 'footer.php';
+    ?>
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+</body>
+
+</html>
